f34f9caa1929e0720f2130057751a3b8c194a297
TRUVIHIU\hieup <top1taker@gmail.com>
2022-02-18 08:34:55 +0700

    Bai tap kieu so

---
+++ src/cau2/BaiLam.java
@@ -0,0 +1,46 @@
+package cau2;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BaiLam {
+
+	public static void main(String[] args) {
+		boolean isValid;
+		do {
+			try {
+				isValid = true;
+				Scanner input = new Scanner(System.in);
+
+				System.out.print("Nhap a: ");
+				int a = input.nextInt();
+				
+				System.out.print("Nhap b: ");
+				int b = input.nextInt();
+				
+				System.out.print("a + b = ");
+				System.out.println(a + b);
+
+				System.out.print("a - b = ");
+				System.out.println(a - b);
+
+				System.out.print("a * b = ");
+				System.out.println(a * b);
+
+				System.out.print("a / b = ");
+				System.out.println(a / b);
+
+				System.out.print("a % b = ");
+				System.out.println(a % b);
+
+				System.out.print("a ^ b = ");
+				System.out.println(Math.pow(a, b));
+
+				input.close();
+			} catch(InputMismatchException error) {
+				isValid = false;
+				System.out.println("Sai kieu du lieu, hay thu lai!");
+			}
+		} while(!isValid);
+	}
+}
---
+++ src/cau3/BaiLam.java
@@ -0,0 +1,46 @@
+package cau3;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class BaiLam {
+
+	public static void main(String[] args) {
+		boolean isValid;
+		do {
+			try {
+				isValid = true;
+				Scanner input = new Scanner(System.in);
+
+				System.out.print("Nhap a: ");
+				double a = input.nextDouble();
+				
+				System.out.print("Nhap b: ");
+				double b = input.nextDouble();
+				
+				System.out.print("a + b = ");
+				System.out.println(a + b);
+
+				System.out.print("a - b = ");
+				System.out.println(a - b);
+
+				System.out.print("a * b = ");
+				System.out.println(a * b);
+
+				System.out.print("a / b = ");
+				System.out.println(a / b);
+
+				System.out.print("a % b = ");
+				System.out.println(a % b);
+
+				System.out.print("a ^ b = ");
+				System.out.println(Math.pow(a, b));
+
+				input.close();
+			} catch(InputMismatchException error) {
+				isValid = false;
+				System.out.println("Sai kieu du lieu, hay thu lai!");
+			}
+		} while(!isValid);
+	}
+}
